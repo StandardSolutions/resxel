a66ea0f58b740a0ed4fa8b0737e6746fc8ad55ee
Merkurev Sergei <merkurevsergei@yandex.ru>
2025-11-12 00:52:25 +0900

    [RXL-19] Implement Location

---
+++ src/main/java/com/stdsolutions/resxel/Location.java
@@ -0,0 +1,42 @@
+package com.stdsolutions.resxel;
+
+import java.net.URI;
+import java.nio.file.Path;
+
+public final class Location {
+
+    private static final String DEFAULT_LOCATION = "null:/";
+
+    private final String value;
+
+    public Location(final String root, final String dir) {
+        this.value = root == null || dir == null ? DEFAULT_LOCATION : root + "!/" + dir;
+    }
+
+    public Location(final URI uri) {
+        this.value = uri == null ? DEFAULT_LOCATION : uri.toString();
+
+    }
+
+    public String scheme() {
+        final int idx = value.indexOf(":/");
+        if (idx < 2) {
+            return "file";
+        }
+        return value.substring(0, idx);
+    }
+
+    public Path root() {
+        final int idx = value.indexOf("!/");
+        final String root = idx == -1 ? value : value.substring(0, idx);
+        return Path.of(root);
+    }
+
+    public String dir() {
+        final int idx = value.indexOf("!/");
+        if (idx == -1) {
+            return "/";
+        }
+        return value.substring(idx + 1);
+    }
+}
